ecce731a3079018c09a3cb80c253833c40e23a6b
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-27 17:23:06 +0200

    Creada clase parametro e importada clase controller

---
+++ PokeApi/src/main/java/PokeApi/example/PokeApi/Controller.java
@@ -0,0 +1,31 @@
+package PokeApi.example.PokeApi;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/registro")
+    public ArrayList<pokemon> listaObjeto1()
+    {
+        LeerJson reader = new LeerJson();
+        //Leemos el reason e introducimos los elementos en un arraylist
+        ArrayList<pokemon> lista = reader.LeerFicheroJson1("Criptomonedas.json");
+        //Mostramos los elementos leidos
+        return lista;
+    }
+/*
+    @PostMapping("/peticion")
+    public ArrayList<Criptomoneda> create(@RequestBody Criptomoneda objeto1)
+    {
+        DataHanding dataHanding = new DataHanding();
+        ArrayList<Criptomoneda> lista;
+        lista = dataHanding.AnadirObjeto1(objeto1,"Criptomonedas.json");
+        EscribirJson escribirJSON = new EscribirJson();
+        escribirJSON.escribirObjeto1(lista);
+        return lista;
+    }
+*/
+}
