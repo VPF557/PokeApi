734d1080e47e4dbf8ef55bcf10b538dc33d8ccf7
VPF557 <pedrojosepaniagua01@gmail.com>
2023-06-27 17:09:44 +0200

    Añadido comentarios aclaratorios

--- PokeApi/src/main/java/PokeApi/example/PokeApi/PokeApiApplication.java
+++ PokeApi/src/main/java/PokeApi/example/PokeApi/PokeApiApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PokeApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(PokeApiApplication.class, args);
+		//Ejemplo swapi con PokeApi
 	}
 
 }
